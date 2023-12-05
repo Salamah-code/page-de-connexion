a3817eaedb4df3e9f90d1e65b006d7d728aaaa6f
Salamah-code <diarrasalama20@gmail.com>
2023-12-05 12:48:13 +0100

    boostrap connexion page

--- Admin/connexion.php
+++ Admin/connexion.php
@@ -85,7 +85,7 @@ if (isset($_POST['username'])){
 	$rows = mysqli_num_rows($result);
 	if($rows==1){
 	    $_SESSION['username'] = $username;
-	    header("Location: ../index.php");
+	    header("Location: ../accueil.php");
 	}else{
 		$message = "Le nom d'utilisateur ou le mot de passe est incorrect.";
 	}
